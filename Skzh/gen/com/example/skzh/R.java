a09b470bd8d1e9ffffd573e54ce911fae8caf0ac
KevinBetterQ <1093850932@qq.com>
2016-07-20 19:49:50 +0800

    Finash without Wechat

--- Skzh/gen/com/example/skzh/R.java
+++ Skzh/gen/com/example/skzh/R.java
@@ -186,17 +186,19 @@ public final class R {
         public static final int zheng_selector=0x7f02009e;
     }
     public static final class id {
-        public static final int action_settings=0x7f090019;
+        public static final int action_settings=0x7f09001b;
+        public static final int analogClock1=0x7f09001a;
         public static final int button1=0x7f090000;
-        public static final int checkBox=0x7f090015;
-        public static final int et_guang=0x7f090018;
-        public static final int et_wendu=0x7f090017;
+        public static final int checkBox=0x7f090016;
+        public static final int digitalClock1=0x7f09000d;
+        public static final int et_guang=0x7f090019;
+        public static final int et_wendu=0x7f090018;
         public static final int etalarmset=0x7f090001;
-        public static final int pwmadd=0x7f090013;
-        public static final int pwmjj=0x7f090014;
-        public static final int smart_btnback=0x7f090011;
-        public static final int smart_btnforward=0x7f09000f;
-        public static final int smart_btnstop=0x7f090010;
+        public static final int pwmadd=0x7f090014;
+        public static final int pwmjj=0x7f090015;
+        public static final int smart_btnback=0x7f090012;
+        public static final int smart_btnforward=0x7f090010;
+        public static final int smart_btnstop=0x7f090011;
         public static final int smarthome_humi=0x7f090005;
         public static final int smarthome_light=0x7f090008;
         public static final int smarthome_lx=0x7f09000a;
@@ -208,10 +210,10 @@ public final class R {
         public static final int smarthome_sheshidu=0x7f090004;
         public static final int smarthome_smoketext=0x7f09000b;
         public static final int smarthome_tempe=0x7f090002;
-        public static final int smarthomeengin=0x7f09000d;
-        public static final int xt_buguang=0x7f090012;
-        public static final int xt_moshi=0x7f090016;
-        public static final int xt_tongfeng=0x7f09000e;
+        public static final int smarthomeengin=0x7f09000e;
+        public static final int xt_buguang=0x7f090013;
+        public static final int xt_moshi=0x7f090017;
+        public static final int xt_tongfeng=0x7f09000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
